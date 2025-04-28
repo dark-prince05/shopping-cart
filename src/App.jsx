9ac80ff190b2a6cefb30d25019bd2a3d669c0ea9
Dark-prince <barathraj0504@gmail.com>
2025-04-28 15:44:30 +0530

    Implement Page Details

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from "react";
 import "./App.css";
 import Header from "./components/Header.jsx";
-import Main from "./components/Main.jsx";
+import MainPage from "./components/MainPage.jsx";
+import PageDetails from "./components/PageDetails.jsx";
 
 function App() {
   return (
     <>
       <Header />
-      <Main />
+      <MainPage />
+      <PageDetails />
     </>
   );
 }
